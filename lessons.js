6cce68b76921b25286987d1f83def5e9086e38c6
rasaqakewusure <74925863+rasaqakewusure@users.noreply.github.com>
2022-10-30 11:13:08 +0000

    Adding more lessons

--- lessons.js
+++ lessons.js
@@ -1,3 +1,4 @@
+// Lesson 1 
 var webstore=  new Vue({
     el: '#app',
     data:{
@@ -8,6 +9,7 @@ var webstore=  new Vue({
             price: 100,
             spaces: 5
         },
+        // 
         cart:[]
         },
         methods: { 
@@ -25,12 +27,12 @@ var webstore=  new Vue({
             }
           }
 }); 
-
+// Lesson 2 
 var webstore=  new Vue({
     el: '#app2',
     data:{
         subject2:{
-            id: 1001,
+            id: 1002,
             subjects: "Math",
             location: "Oxford",
             price: 100,
@@ -53,3 +55,119 @@ var webstore=  new Vue({
             }
           }
 });
+// Lesson 3 
+var webstore=  new Vue({
+  el: '#app3',
+  data:{
+      subject3:{
+          id: 1003,
+          subjects: "English",
+          location: "London",
+          price: 100,
+          spaces: 5
+      },
+      cart:[]
+      },
+      methods: { 
+          addToCart: function () {
+              this.cart.push(this.subject3.id);
+}
+      },
+      computed: {
+      
+          cartItemCount() {
+            return this.cart.length || ''
+          },
+          canAddClass() {
+            return this.subject3.spaces > this.cartItemCount;
+          }
+        }
+});
+
+// Lesson 4 
+var webstore=  new Vue({
+  el: '#app4',
+  data:{
+      subject4:{
+          id: 1003,
+          subjects: "English",
+          location: "York",
+          price: 80,
+          spaces: 5
+      },
+      cart:[]
+      },
+      methods: { 
+          addToCart: function () {
+              this.cart.push(this.subject4.id);
+}
+      },
+      computed: {
+      
+          cartItemCount() {
+            return this.cart.length || ''
+          },
+          canAddClass() {
+            return this.subject4.spaces > this.cartItemCount;
+          }
+        }
+});
+
+// Lesson 5
+var webstore=  new Vue({
+  el: '#app5',
+  data:{
+      subject5:{
+          id: 1003,
+          subjects: "Music",
+          location: "Bristol",
+          price: 80,
+          spaces: 5
+      },
+      cart:[]
+      },
+      methods: { 
+          addToCart: function () {
+              this.cart.push(this.subject5.id);
+}
+      },
+      computed: {
+      
+          cartItemCount() {
+            return this.cart.length || ''
+          },
+          canAddClass() {
+            return this.subject5.spaces > this.cartItemCount;
+          }
+        }
+});
+
+// Lesson 6
+var webstore=  new Vue({
+  el: '#app6',
+  data:{
+      subject6:{
+          id: 1003,
+          subjects: "Music",
+          location: "Bristol",
+          price: 90,
+          spaces: 5
+      },
+      cart:[]
+      },
+      methods: { 
+          addToCart: function () {
+              this.cart.push(this.subject6.id);
+}
+      },
+      computed: {
+      
+          cartItemCount() {
+            return this.cart.length || ''
+          },
+          canAddClass() {
+            return this.subject6.spaces > this.cartItemCount;
+          }
+        }
+});
+
